d0e54d40b62c4185e4d32f392cff5dfcf5a99007
LAK132 <lakridesagain@gmail.com>
2025-07-27 11:45:44 +0930

    reproduce the actual license text as necessary

--- src/main.cpp
+++ src/main.cpp
@@ -539,16 +539,31 @@ struct main_window : lak::basic_window<main_window>
 			LAK_TREE_NODE("ImGui")
 			{
 				ImGui::Text("https://github.com/ocornut/imgui");
+				ImGui::Text(R"(Copyright (c) 2014-2022 Omar Cornut
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+)");
 			}
 			LAK_TREE_NODE("gl3w") { ImGui::Text("https://github.com/skaslev/gl3w"); }
 #ifdef LAK_USE_SDL
 			LAK_TREE_NODE("SDL2") { ImGui::Text("https://www.libsdl.org/"); }
 #endif
-			LAK_TREE_NODE("tinflate")
-			{
-				ImGui::Text("http://achurch.org/tinflate.c");
-				ImGui::Text("Fork: https://github.com/LAK132/tinflate");
-			}
 			LAK_TREE_NODE("LibRaw")
 			{
 				ImGui::Text("https://github.com/LibRaw/LibRaw");
@@ -558,7 +573,29 @@ struct main_window : lak::basic_window<main_window>
 				ImGui::Text(
 				  "https://github.com/nothings/stb/blob/master/stb_image_write.h");
 			}
-			LAK_TREE_NODE("glm") { ImGui::Text("https://github.com/g-truc/glm"); }
+			LAK_TREE_NODE("glm")
+			{
+				ImGui::Text("https://github.com/g-truc/glm");
+				ImGui::Text(R"(Copyright (c) 2005 - G-Truc Creation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.)");
+			}
 			LAK_TREE_NODE("lak") { ImGui::Text("https://github.com/LAK132/lak"); }
 		}
 	}
