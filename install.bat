6636a7dbc3d22abee7bc42773a7439673cd14a14
LAK132 <lakridesagain@gmail.com>
2026-06-21 00:50:54 +0930

    only attempt to install runtime stuff

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
 @echo off
 rmdir /s /q %CD%\install
-meson install -C build --destdir %CD%\install %* || exit 1
+meson install -C build --destdir %CD%\install --no-rebuild --tags=runtime %* || exit 1
--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 rm -rf $PWD/install
-meson install -C build --destdir $PWD/install $* || exit 1
+meson install -C build --destdir $PWD/install --no-rebuild --tags=runtime $* || exit 1
