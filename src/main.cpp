1bb1fb4cca48abd435f096531e450ac2da4640cc
LAK132 <lakridesagain@gmail.com>
2026-08-09 06:21:53 +0930

    improve demosaicing

--- src/main.cpp
+++ src/main.cpp
@@ -216,7 +216,9 @@ void process_image_white_level(lak::tasks &tasks,
 				  const size_t i = iy + x;
 
 				  img[i].r = float(lraw->imgdata.image[i][0]) / white_level;
-				  img[i].g = float(lraw->imgdata.image[i][1]) / white_level;
+				  img[i].g = std::max(float(lraw->imgdata.image[i][1]),
+				                      float(lraw->imgdata.image[i][3])) /
+				             white_level;
 				  img[i].b = float(lraw->imgdata.image[i][2]) / white_level;
 			  }
 		  });
@@ -270,37 +272,287 @@ void process_image_demosaic(lak::tasks &tasks,
 	}
 	else if (format == sensor_format_t::xtrans)
 	{
-		isize.x /= 3U;
-		isize.y /= 3U;
+		isize.x -= (3U * 2U);
+		isize.y -= (3U * 2U);
 		dst.resize(flip4(isize));
 
-		for (size_t y = 0; y < isize.y; ++y)
+		for (size_t y = 0; y < isize.y / 3U; ++y)
 		{
 			tasks.push(
 			  [&, y = y]()
 			  {
 				  const size_t y3 = y * 3;
-				  for (size_t x = 0; x < isize.x; ++x)
+				  for (size_t x = 0; x < isize.x / 3U; ++x)
 				  {
-					  const size_t x3       = x * 3;
-					  const lak::vec2s_t xy = flip124({x, y}, dst.size());
+					  const size_t x3 = x * 3;
 
-					  const bool xtrans_even_cell = (x + y) % 2U == 0U;
-					  const size_t xtrans_off1    = xtrans_even_cell ? 0U : 1U;
-					  const size_t xtrans_off2    = xtrans_even_cell ? 1U : 0U;
+					  const lak::vec2s_t xy_dst_00 =
+					    flip124({x3 + 0U, y3 + 0U}, dst.size());
+					  const lak::vec2s_t xy_dst_01 =
+					    flip124({x3 + 0U, y3 + 1U}, dst.size());
+					  const lak::vec2s_t xy_dst_02 =
+					    flip124({x3 + 0U, y3 + 2U}, dst.size());
 
-					  dst[xy].r = (src[{x3 + 2U, y3 + xtrans_off1}].r +
-					               src[{x3 + xtrans_off2, y3 + 2U}].r) /
-					              2.f;
+					  const lak::vec2s_t xy_dst_10 =
+					    flip124({x3 + 1U, y3 + 0U}, dst.size());
+					  const lak::vec2s_t xy_dst_11 =
+					    flip124({x3 + 1U, y3 + 1U}, dst.size());
+					  const lak::vec2s_t xy_dst_12 =
+					    flip124({x3 + 1U, y3 + 2U}, dst.size());
 
-					  dst[xy].g =
-					    (src[{x3, y3}].g + src[{x3 + 1U, y3}].g + src[{x3, y3 + 1U}].g +
-					     src[{x3 + 1U, y3 + 1U}].g + src[{x3 + 2U, y3 + 2U}].g) /
-					    5.f;
+					  const lak::vec2s_t xy_dst_20 =
+					    flip124({x3 + 2U, y3 + 0U}, dst.size());
+					  const lak::vec2s_t xy_dst_21 =
+					    flip124({x3 + 2U, y3 + 1U}, dst.size());
+					  const lak::vec2s_t xy_dst_22 =
+					    flip124({x3 + 2U, y3 + 2U}, dst.size());
 
-					  dst[xy].b = (src[{x3 + 2U, y3 + xtrans_off2}].b +
-					               src[{x3 + xtrans_off1, y3 + 2U}].b) /
-					              2.f;
+					  const lak::vec2s_t xy_src_01_20{x3 + 0U + 2U, y3 + 3U + 0U};
+					  const lak::vec2s_t xy_src_01_21{x3 + 0U + 2U, y3 + 3U + 1U};
+					  const lak::vec2s_t xy_src_01_22{x3 + 0U + 2U, y3 + 3U + 2U};
+
+					  const lak::vec2s_t xy_src_02_20{x3 + 0U + 2U, y3 + 6U + 0U};
+
+					  const lak::vec2s_t xy_src_10_02{x3 + 3U + 0U, y3 + 0U + 2U};
+					  const lak::vec2s_t xy_src_10_12{x3 + 3U + 1U, y3 + 0U + 2U};
+					  const lak::vec2s_t xy_src_10_22{x3 + 3U + 2U, y3 + 0U + 2U};
+
+					  const lak::vec2s_t xy_src_11_00{x3 + 3U + 0U, y3 + 3U + 0U};
+					  const lak::vec2s_t xy_src_11_01{x3 + 3U + 0U, y3 + 3U + 1U};
+					  const lak::vec2s_t xy_src_11_02{x3 + 3U + 0U, y3 + 3U + 2U};
+					  const lak::vec2s_t xy_src_11_10{x3 + 3U + 1U, y3 + 3U + 0U};
+					  const lak::vec2s_t xy_src_11_11{x3 + 3U + 1U, y3 + 3U + 1U};
+					  const lak::vec2s_t xy_src_11_12{x3 + 3U + 1U, y3 + 3U + 2U};
+					  const lak::vec2s_t xy_src_11_20{x3 + 3U + 2U, y3 + 3U + 0U};
+					  const lak::vec2s_t xy_src_11_21{x3 + 3U + 2U, y3 + 3U + 1U};
+					  const lak::vec2s_t xy_src_11_22{x3 + 3U + 2U, y3 + 3U + 2U};
+
+					  const lak::vec2s_t xy_src_12_00{x3 + 3U + 0U, y3 + 6U + 0U};
+					  const lak::vec2s_t xy_src_12_10{x3 + 3U + 1U, y3 + 6U + 0U};
+					  const lak::vec2s_t xy_src_12_20{x3 + 3U + 2U, y3 + 6U + 0U};
+
+					  const lak::vec2s_t xy_src_20_02{x3 + 6U + 0U, y3 + 0U + 2U};
+
+					  const lak::vec2s_t xy_src_21_00{x3 + 6U + 0U, y3 + 3U + 0U};
+					  const lak::vec2s_t xy_src_21_01{x3 + 6U + 0U, y3 + 3U + 1U};
+					  const lak::vec2s_t xy_src_21_02{x3 + 6U + 0U, y3 + 3U + 2U};
+
+					  /*
+					  G G B  G G R  G G B  G G R
+					  G G R  G G B  G G R  G G B
+					  R B G  B R G  R B G  B R G
+
+					  G G R  G G B  G G R  G G B
+					  G G B  G G R  G G B  G G R
+					  B R G  R B G  B R G  R B G
+
+					  G G B  G G R  G G B  G G R
+					  G G R  G G B  G G R  G G B
+					  R B G  B R G  R B G  B R G
+
+					  G G R  G G B  G G R  G G B
+					  G G B  G G R  G G B  G G R
+					  B R G  R B G  B R G  R B G
+					  */
+
+					  /*
+					  G G .  G G .  G G .
+					  G G .  G G .  G G .
+					  . . G  . . G  . . G
+
+					  G G .  G G .  G G .
+					  G G .  G G .  G G .
+					  . . G  . . G  . . G
+
+					  G G .  G G .  G G .
+					  G G .  G G .  G G .
+					  . . G  . . G  . . G
+					  */
+
+					  dst[xy_dst_00].g = src[xy_src_11_00].g;
+					  dst[xy_dst_10].g = src[xy_src_11_10].g;
+					  dst[xy_dst_20].g = (src[xy_src_10_22].g + src[xy_src_11_10].g +
+					                      src[xy_src_21_00].g) /
+					                     3.f;
+
+					  dst[xy_dst_01].g = src[xy_src_11_01].g;
+					  dst[xy_dst_11].g = src[xy_src_11_11].g;
+					  dst[xy_dst_21].g = (src[xy_src_11_22].g + src[xy_src_11_11].g +
+					                      src[xy_src_21_01].g) /
+					                     3.f;
+
+					  dst[xy_dst_02].g = (src[xy_src_01_22].g + src[xy_src_11_01].g +
+					                      src[xy_src_12_00].g) /
+					                     3.f;
+					  dst[xy_dst_12].g = (src[xy_src_11_22].g + src[xy_src_11_11].g +
+					                      src[xy_src_12_10].g) /
+					                     3.f;
+					  dst[xy_dst_22].g = src[xy_src_11_22].g;
+
+					  constexpr float nb4_4 = 0.05f;
+					  constexpr float nb4_3 = 0.1f;
+					  constexpr float nb4_2 = 0.2f;
+					  constexpr float nb4_1 = 1.f - (nb4_2 + nb4_3 + nb4_4);
+
+					  constexpr float nb3_2 = 0.2f;
+					  constexpr float nb3_1 = 1.f - (nb3_2 + nb3_2);
+
+					  if (((x + y) % 2U) == 0U)
+					  {
+						  /*
+						  . . .  . . B  . . .
+						  . . B  . . .  . . B
+						  B . .  . B .  B . .
+
+						  . . B  . . .  . . B
+						  . . .  . . B  . . .
+						  . B .  B . .  . B .
+
+						  . . .  . . B  . . .
+						  . . B  . . .  . . B
+						  B . .  . B .  B . .
+						  */
+						  dst[xy_dst_00].b =
+						    (nb4_1 * src[xy_src_01_20].b) + (nb4_2 * src[xy_src_10_12].b) +
+						    (nb4_3 * src[xy_src_11_02].b) + (nb4_4 * src[xy_src_11_21].b);
+						  dst[xy_dst_10].b =
+						    (nb4_1 * src[xy_src_10_12].b) + (nb4_2 * src[xy_src_11_21].b) +
+						    (nb4_3 * src[xy_src_01_20].b) + (nb4_4 * src[xy_src_11_02].b);
+						  dst[xy_dst_20].b = (nb3_1 * src[xy_src_11_21].b) +
+						                     (nb3_2 * src[xy_src_10_12].b) +
+						                     (nb3_2 * src[xy_src_20_02].b);
+
+						  dst[xy_dst_01].b =
+						    (nb4_1 * src[xy_src_11_02].b) + (nb4_2 * src[xy_src_01_20].b) +
+						    (nb4_3 * src[xy_src_11_21].b) + (nb4_4 * src[xy_src_10_12].b);
+						  dst[xy_dst_11].b =
+						    (nb4_1 * src[xy_src_11_21].b) + (nb4_2 * src[xy_src_11_02].b) +
+						    (nb4_3 * src[xy_src_10_12].b) + (nb4_4 * src[xy_src_01_20].b);
+						  dst[xy_dst_21].b = src[xy_src_11_21].b;
+
+						  dst[xy_dst_02].b = src[xy_src_11_02].b;
+						  dst[xy_dst_12].b = (nb3_1 * src[xy_src_11_02].b) +
+						                     (nb3_2 * src[xy_src_11_21].b) +
+						                     (nb3_2 * src[xy_src_12_20].b);
+						  dst[xy_dst_22].b =
+						    (src[xy_src_11_21].b + src[xy_src_12_20].b) / 2.f;
+
+						  /*
+						  . . R  . . .  . . R
+						  . . .  . . R  . . .
+						  . R .  R . .  . R .
+
+						  . . .  . . R  . . .
+						  . . R  . . .  . . R
+						  R . .  . R .  R . .
+
+						  . . R  . . .  . . R
+						  . . .  . . R  . . .
+						  . R .  R . .  . R .
+						  */
+						  dst[xy_dst_00].r =
+						    (nb4_1 * src[xy_src_10_02].r) + (nb4_2 * src[xy_src_01_21].r) +
+						    (nb4_3 * src[xy_src_11_20].r) + (nb4_4 * src[xy_src_11_12].r);
+						  dst[xy_dst_10].r =
+						    (nb4_1 * src[xy_src_11_20].r) + (nb4_2 * src[xy_src_10_02].r) +
+						    (nb4_3 * src[xy_src_11_12].r) + (nb4_4 * src[xy_src_01_21].r);
+						  dst[xy_dst_20].r = src[xy_src_11_20].r;
+
+						  dst[xy_dst_01].r =
+						    (nb4_1 * src[xy_src_01_21].r) + (nb4_2 * src[xy_src_11_12].r) +
+						    (nb4_3 * src[xy_src_10_02].r) + (nb4_4 * src[xy_src_11_20].r);
+						  dst[xy_dst_11].r =
+						    (nb4_1 * src[xy_src_11_12].r) + (nb4_2 * src[xy_src_11_20].r) +
+						    (nb4_3 * src[xy_src_01_21].r) + (nb4_4 * src[xy_src_10_02].r);
+						  dst[xy_dst_21].r = (nb3_1 * src[xy_src_11_20].r) +
+						                     (nb3_2 * src[xy_src_11_12].r) +
+						                     (nb3_2 * src[xy_src_21_02].r);
+
+						  dst[xy_dst_02].r = (nb3_1 * src[xy_src_11_12].r) +
+						                     (nb3_2 * src[xy_src_01_21].r) +
+						                     (nb3_2 * src[xy_src_02_20].r);
+						  dst[xy_dst_12].r = src[xy_src_11_12].r;
+						  dst[xy_dst_22].r =
+						    (src[xy_src_11_12].r + src[xy_src_21_02].r) / 2.f;
+					  }
+					  else
+					  {
+						  /*
+						  . . B  . . .  . . B
+						  . . .  . . B  . . .
+						  . B .  B . .  . B .
+
+						  . . .  . . B  . . .
+						  . . B  . . .  . . B
+						  B . .  . B .  B . .
+
+						  . . B  . . .  . . B
+						  . . .  . . B  . . .
+						  . B .  B . .  . B .
+						  */
+						  dst[xy_dst_00].b =
+						    (nb4_1 * src[xy_src_10_02].b) + (nb4_2 * src[xy_src_01_21].b) +
+						    (nb4_3 * src[xy_src_11_20].b) + (nb4_4 * src[xy_src_11_12].b);
+						  dst[xy_dst_10].b =
+						    (nb4_1 * src[xy_src_11_20].b) + (nb4_2 * src[xy_src_10_02].b) +
+						    (nb4_3 * src[xy_src_11_12].b) + (nb4_4 * src[xy_src_01_21].b);
+						  dst[xy_dst_20].b = src[xy_src_11_20].b;
+
+						  dst[xy_dst_01].b =
+						    (nb4_1 * src[xy_src_01_21].b) + (nb4_2 * src[xy_src_11_12].b) +
+						    (nb4_3 * src[xy_src_10_02].b) + (nb4_4 * src[xy_src_11_20].b);
+						  dst[xy_dst_11].b =
+						    (nb4_1 * src[xy_src_11_12].b) + (nb4_2 * src[xy_src_11_20].b) +
+						    (nb4_3 * src[xy_src_01_21].b) + (nb4_4 * src[xy_src_10_02].b);
+						  dst[xy_dst_21].b = (nb3_1 * src[xy_src_11_20].b) +
+						                     (nb3_2 * src[xy_src_11_12].b) +
+						                     (nb3_2 * src[xy_src_21_02].b);
+
+						  dst[xy_dst_02].b = (nb3_1 * src[xy_src_11_12].b) +
+						                     (nb3_2 * src[xy_src_01_21].b) +
+						                     (nb3_2 * src[xy_src_02_20].b);
+						  dst[xy_dst_12].b = src[xy_src_11_12].b;
+						  dst[xy_dst_22].b =
+						    (src[xy_src_11_12].b + src[xy_src_21_02].b) / 2.f;
+
+						  /*
+						  . . .  . . R  . . .
+						  . . R  . . .  . . R
+						  R . .  . R .  R . .
+
+						  . . R  . . .  . . R
+						  . . .  . . R  . . .
+						  . R .  R . .  . R .
+
+						  . . .  . . R  . . .
+						  . . R  . . .  . . R
+						  R . .  . R .  R . .
+						  */
+						  dst[xy_dst_00].r =
+						    (nb4_1 * src[xy_src_01_20].r) + (nb4_2 * src[xy_src_10_12].r) +
+						    (nb4_3 * src[xy_src_11_02].r) + (nb4_4 * src[xy_src_11_21].r);
+						  dst[xy_dst_10].r =
+						    (nb4_1 * src[xy_src_10_12].r) + (nb4_2 * src[xy_src_11_21].r) +
+						    (nb4_3 * src[xy_src_01_20].r) + (nb4_4 * src[xy_src_11_02].r);
+						  dst[xy_dst_20].r = (nb3_1 * src[xy_src_11_21].r) +
+						                     (nb3_2 * src[xy_src_10_12].r) +
+						                     (nb3_2 * src[xy_src_20_02].r);
+
+						  dst[xy_dst_01].r =
+						    (nb4_1 * src[xy_src_11_02].r) + (nb4_2 * src[xy_src_01_20].r) +
+						    (nb4_3 * src[xy_src_11_21].r) + (nb4_4 * src[xy_src_10_12].r);
+						  dst[xy_dst_11].r =
+						    (nb4_1 * src[xy_src_11_21].r) + (nb4_2 * src[xy_src_11_02].r) +
+						    (nb4_3 * src[xy_src_10_12].r) + (nb4_4 * src[xy_src_01_20].r);
+						  dst[xy_dst_21].r = src[xy_src_11_21].r;
+
+						  dst[xy_dst_02].r = src[xy_src_11_02].r;
+						  dst[xy_dst_12].r = (nb3_1 * src[xy_src_11_02].r) +
+						                     (nb3_2 * src[xy_src_11_21].r) +
+						                     (nb3_2 * src[xy_src_12_20].r);
+						  dst[xy_dst_22].r =
+						    (src[xy_src_11_21].r + src[xy_src_12_20].r) / 2.f;
+					  }
 				  }
 			  });
 		}
@@ -309,8 +561,8 @@ void process_image_demosaic(lak::tasks &tasks,
 	}
 	else
 	{
-		isize.x /= 2U;
-		isize.y /= 2U;
+		isize.x -= (2U * 2U);
+		isize.y -= (2U * 2U);
 		dst.resize(flip4(isize));
 
 		lak::vec2s_t channels[4U] = {{0U, 0U}, {0U, 0U}, {0U, 0U}, {0U, 0U}};
@@ -324,17 +576,106 @@ void process_image_demosaic(lak::tasks &tasks,
 			tasks.push(
 			  [&, y = y]()
 			  {
-				  const size_t y2 = y * 2;
 				  for (size_t x = 0; x < isize.x; ++x)
 				  {
-					  const size_t x2           = x * 2;
 					  const lak::vec2s_t xy_dst = flip124({x, y}, dst.size());
-					  const lak::vec2s_t xy_src{x2, y2};
+					  const lak::vec2s_t xy_src{x + 2U, y + 2U};
 
-					  dst[xy_dst].r = src[xy_src + channels[0U]].r;
-					  dst[xy_dst].g = std::max(src[xy_src + channels[1U]].g,
-					                           src[xy_src + channels[3U]].g);
-					  dst[xy_dst].b = src[xy_src + channels[2U]].b;
+					  /*
+					  R G R G
+					  G B G B
+					  R G R G
+					  G B G B
+					  */
+
+					  /*
+					  R . R .
+					  . . . .
+					  R . R .
+					  . . . .
+					  */
+
+					  if (bool r_x = ((x % 2U) == channels[0U].x),
+					      r_y      = ((y % 2U) == channels[0U].y);
+					      r_x && r_y) [[unlikely]]
+					  {
+						  dst[xy_dst].r = src[xy_src].r;
+					  }
+					  else if (!(r_x || r_y))
+					  {
+						  dst[xy_dst].r = (src[{xy_src.x - 1U, xy_src.y - 1U}].r +
+						                   src[{xy_src.x - 1U, xy_src.y + 1U}].r +
+						                   src[{xy_src.x + 1U, xy_src.y - 1U}].r +
+						                   src[{xy_src.x + 1U, xy_src.y + 1U}].r) /
+						                  4.f;
+					  }
+					  else if (r_x)
+					  {
+						  dst[xy_dst].r = (src[{xy_src.x, xy_src.y - 1U}].r +
+						                   src[{xy_src.x, xy_src.y + 1U}].r) /
+						                  2.f;
+					  }
+					  else /* if (r_y) */
+					  {
+						  dst[xy_dst].r = (src[{xy_src.x - 1U, xy_src.y}].r +
+						                   src[{xy_src.x + 1U, xy_src.y}].r) /
+						                  2.f;
+					  }
+
+					  /*
+					  . . . .
+					  . B . B
+					  . . . .
+					  . B . B
+					  */
+
+					  if (bool b_x = ((x % 2U) == channels[2U].x),
+					      b_y      = ((y % 2U) == channels[2U].y);
+					      b_x && b_y) [[unlikely]]
+					  {
+						  dst[xy_dst].b = src[xy_src].b;
+					  }
+					  else if (!(b_x || b_y))
+					  {
+						  dst[xy_dst].b = (src[{xy_src.x - 1U, xy_src.y - 1U}].b +
+						                   src[{xy_src.x - 1U, xy_src.y + 1U}].b +
+						                   src[{xy_src.x + 1U, xy_src.y - 1U}].b +
+						                   src[{xy_src.x + 1U, xy_src.y + 1U}].b) /
+						                  4.f;
+					  }
+					  else if (b_x)
+					  {
+						  dst[xy_dst].b = (src[{xy_src.x, xy_src.y - 1U}].b +
+						                   src[{xy_src.x, xy_src.y + 1U}].b) /
+						                  2.f;
+					  }
+					  else /* if (b_y) */
+					  {
+						  dst[xy_dst].b = (src[{xy_src.x - 1U, xy_src.y}].b +
+						                   src[{xy_src.x + 1U, xy_src.y}].b) /
+						                  2.f;
+					  }
+
+					  /*
+					  . G . G
+					  G . G .
+					  . G . G
+					  G . G .
+					  */
+
+					  if (lak::vec2s_t xy_m2{xy_src.x % 2U, xy_src.y % 2U};
+					      (xy_m2 == channels[1U]) || (xy_m2 == channels[3U]))
+					  {
+						  dst[xy_dst].g = src[xy_src].g;
+					  }
+					  else
+					  {
+						  dst[xy_dst].g = (src[{xy_src.x - 1U, xy_src.y}].g +
+						                   src[{xy_src.x + 1U, xy_src.y}].g +
+						                   src[{xy_src.x, xy_src.y - 1U}].g +
+						                   src[{xy_src.x, xy_src.y + 1U}].g) /
+						                  4.f;
+					  }
 				  }
 			  });
 		}
